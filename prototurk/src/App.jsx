ef8f146bd676fb42cb811780940b602601604897
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-11-27 17:27:15 +0300

    useRef() ve forwardRef()

--- prototurk/src/App.jsx
+++ prototurk/src/App.jsx
@@ -1,11 +1,31 @@
 
 import './App.css'
 import Test from './components/Test.jsx'
-import { useState } from "react";
+import { useState,useRef, forwardRef } from "react";
+
+  //input compnent olarak bu fn yazdık 
+  // function Input (props,ref) {
+  //   return <input ref={ref} type='text' {...props}/>
+  // }
+
+  // Input = forwardRef(Input)
+
+  // yada  alttaki gibi 
+  const Input = forwardRef((props,ref) => {
+    return <input ref={ref} type='text' {...props}/>
+
+  })
 
 function App() {
 
   const [show, setShow] = useState(false);
+
+
+
+  const inputRef = useRef()
+  const focusInput = () => {
+    inputRef.current.focus()
+  }
 
   return (
     <>
@@ -13,6 +33,11 @@ function App() {
       {show ? 'göster' : 'gizle'}
     </button>
     {show && <Test/>}
+
+
+    <h1> useRef - useForward</h1>
+    <Input ref={inputRef} />
+    <button onClick={focusInput}>focusla</button>
     </>
   )
 }
--- prototurk/src/components/Test.jsx
+++ prototurk/src/components/Test.jsx
@@ -3,6 +3,7 @@ import { useEffect,useState  } from "react";
 const Test = () => {
 
   const [count, setCount] = useState(0);
+  const [post, setPost] = useState(false);
 
   useEffect(() => {
     console.log('component ilik yüklendiğinde calsır');
@@ -18,13 +19,21 @@ const Test = () => {
   })
 
   useEffect(() => {
-    console.log('Count değeri değişti ' ,count);
+    fetch(`https://jsonplaceholder.typicode.com/post/${count}`)
+    
+      .then(response => response.json())
+    
+      .then(data => setPost(data))
+    
+      .catch(error => console.log(error));
     
   }, [count]);
 
   return (
     <>
+
     <h3>{count}</h3>
+    {post && JSON.stringify(post)}
       <button onClick={() => setCount(count => count+1)}>Arttırma</button>
         <div>Test</div>
     </>
